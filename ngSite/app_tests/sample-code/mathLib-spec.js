ffddb9fe984b31cee297a14182f7ec9365c0a094
Tim van Elst <tim.van.elst@hroffice.nl>
2015-01-08 09:03:37 +0100

    added ngSite with Jasmine test example
    and removed nodejs project

---
+++ ngSite/app_tests/sample-code/mathLib-spec.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../../app/sample-code/code.js" />
+
+describe('Jasmine', function () {
+    it('will add 5 to number', function () {
+        var res = mathLib.add5(10);
+        expect(res).toEqual(15);
+    });
+});
