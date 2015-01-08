47ba77a073fe7ebdc7879cc9d865ce577087c6d4
Tim van Elst <tim.van.elst@hroffice.nl>
2015-01-08 08:37:51 +0100

    first jasmine test example
    added jasmine and created sample code and jasmine tests

---
+++ Jasmine/app_tests/mathLib-spec.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../app/code.js" />
+
+describe('Jasmine', function () {
+    it('will add 5 to number', function () {
+        var res = mathLib.add5(10);
+        expect(res).toEqual(15);
+    });
+});
