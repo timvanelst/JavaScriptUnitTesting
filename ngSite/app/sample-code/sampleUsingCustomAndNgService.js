80f42607cf44b00ea8f7c5bb52d3d1d1586b4bac
Tim van Elst <tim.van.elst@hroffice.nl>
2015-02-17 15:47:44 +0100

    Added sample of testing a controller with depency to a ng and a custom service

---
+++ ngSite/app/sample-code/sampleUsingCustomAndNgService.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'sampleControllerWithNgService';
+    angular.module('sampleServices', []).controller(controllerId,
+        ['$rootScope', 'utilSvc', sample]);
+
+    function sample($rootScope, utilSvc) {
+        var useOfUtil = utilSvc;
+        $rootScope.title = 'Test Titel';
+
+
+        return { getValue: getValue };
+
+        function getValue(value) {
+            if (useOfUtil.isNumber(value)) {
+                console.log(value + ' is a number');
+            }
+            return value;
+        }
+    }
+})();
---
+++ ngSite/app_tests/sample-code/sampleUsingCustomAndNgService-spec.js
@@ -0,0 +1,102 @@
+﻿/// <reference path="../../Scripts/angular.js"/>
+/// <reference path="../../Scripts/angular-mocks.js"/>
+
+/// <reference path="../../app/sample-code/sampleUsingCustomAndNgService.js"/>
+
+describe('Mocking a service', function () {
+    var mockUtilSvc;
+    var $rootScope;
+
+    function createController() {
+        return $controller('sampleControllerWithNgService',
+                {
+                    $rootScope: $rootScope,
+                    utilSvc: mockUtilSvc
+                });
+    }
+
+    describe('without mocking the behaviour', function () {
+        beforeEach(function() {
+            module('sampleServices', function($provide) {
+                var utilSvcMock = jasmine.createSpyObj('utilSvc', ['isNumber', 'isDate']);
+                $provide.value('utilSvc', utilSvcMock);
+            });
+
+            inject(function (utilSvc) {
+                mockUtilSvc = utilSvc;
+            });
+
+        });
+
+        beforeEach(inject(function (_$controller_, _$rootScope_) {
+            // The injector unwraps the underscores (_) from around the parameter names when matching
+            $controller = _$controller_;
+            $rootScope = _$rootScope_;
+        }));
+
+        it('should be able to verify whether a mocked method is called', function () {
+            var controller = createController();
+            //var controller = $controller('sampleControllerWithNgService',
+            //    {
+            //        $rootScope: $rootScope,
+            //        utilSvc: mockUtilSvc
+            //    });
+            controller.getValue(39);
+            expect(mockUtilSvc.isNumber).toHaveBeenCalled();
+        });
+    });
+
+    describe('with behaviour of mock defined', function () {
+
+        beforeEach(function () {
+            module('sampleServices', function ($provide) {
+                $provide.service('utilSvc', function () {
+                    this.isNumber = jasmine.createSpy("isNumber Spy").and.callFake(function (num) {
+                        return true
+                    });
+
+                    this.isDate = jasmine.createSpy('isDate').and.callFake(function (date) {
+                        return true;
+                    });
+                });
+            });
+
+            inject(function (utilSvc) {
+                mockUtilSvc = utilSvc;
+            });
+        });
+
+        beforeEach(inject(function (_$controller_) {
+            // The injector unwraps the underscores (_) from around the parameter names when matching
+            $controller = _$controller_;
+        }));
+
+        it('should run an actual test', function () {
+            expect(true).toBe(true);
+        });
+
+        it('should be able to set the controller to test', function () {
+
+            var controller = createController();
+            //var controller = $controller('sampleControllerWithNgService', { utilSvc: mockUtilSvc });
+            expect(controller).not.toBeUndefined();
+        });
+
+        it('should be able to get the correct value from the controller we are testing', function () {
+            //Getting reference of the mocked service
+            var controller = createController();
+            //var controller = $controller('sampleController', { utilSvc: mockUtilSvc });
+
+            expect(controller.getValue(5)).toBe(5);
+        });
+
+        it('should be able to verify with what parameter a mocked method is called', function () {
+            var controller = createController();
+            //var controller = $controller('sampleControllerWithNgService', { utilSvc: mockUtilSvc });
+            controller.getValue(39);
+            expect(mockUtilSvc.isNumber).toHaveBeenCalledWith(39);
+        });
+
+
+    });
+})
